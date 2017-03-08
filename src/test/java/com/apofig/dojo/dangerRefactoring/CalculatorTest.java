fbc85498e7fb5a73624716a812b2bd034094969b
alexander.baglay <apofig@gmil.com>
2017-03-08 14:16:42 +0200

    Начал с десятичных тестов

--- src/test/java/com/apofig/dojo/dangerRefactoring/CalculatorTest.java
+++ src/test/java/com/apofig/dojo/dangerRefactoring/CalculatorTest.java
@@ -9,6 +9,14 @@ public class CalculatorTest {
     @Test
     public void shouldCalculateWorks() {
         assertCalculate("1+1", "10", "2");
+        assertCalculate("1+10", "10", "11");
+        assertCalculate("10+1", "10", "11");
+        assertCalculate("10+10", "10", "20");
+        assertCalculate("11+11", "10", "22");
+        assertCalculate("123+456", "10", "579");
+        assertCalculate("0+34", "10", "34");
+
+        assertCalculate("0+34", "10", "34");
     }
 
     private void assertCalculate(String expression, String base, String expected) {
