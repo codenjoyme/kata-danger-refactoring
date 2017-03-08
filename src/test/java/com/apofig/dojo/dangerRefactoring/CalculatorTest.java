093168975889dddba75c8ed452c0eb1525efff8a
alexander.baglay <apofig@gmil.com>
2017-03-08 14:45:04 +0200

    Протестили еще двоичную систему

--- src/test/java/com/apofig/dojo/dangerRefactoring/CalculatorTest.java
+++ src/test/java/com/apofig/dojo/dangerRefactoring/CalculatorTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
 
     @Test
-    public void shouldCalculateWorks() {
+    public void shouldCalculateWorks_when10NumberSystem() {
         assertCalculate("1+1", "10", "2");
         assertCalculate("1+10", "10", "11");
         assertCalculate("10+1", "10", "11");
@@ -15,8 +15,19 @@ public class CalculatorTest {
         assertCalculate("11+11", "10", "22");
         assertCalculate("123+456", "10", "579");
         assertCalculate("0+34", "10", "34");
+        assertCalculate("34+0", "10", "34");
+    }
 
-        assertCalculate("0+34", "10", "34");
+    @Test
+    public void shouldCalculateWorks_when2NumberSystem() {
+        assertCalculate("1+1", "2", "10");
+        assertCalculate("1+10", "2", "11");
+        assertCalculate("10+1", "2", "11");
+        assertCalculate("10+10", "2", "100");
+        assertCalculate("11+11", "2", "110");
+        assertCalculate("101+111", "2", "1100");
+        assertCalculate("0+101", "2", "101");
+        assertCalculate("101+0", "2", "101");
     }
 
     private void assertCalculate(String expression, String base, String expected) {
